888641e25f225d71eefde443fa4d57e9ae9e95c6
RaveTr <yuyuss210@gmail.com>
2023-04-06 16:07:43 +0200

    Initial metadata/project info stuff

---
+++ src/main/java/com/rdm/advancedtotems/AdvancedTotems.java
@@ -0,0 +1,17 @@
+package com.rdm.advancedtotems;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AdvancedTotems implements ModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("advancedtotems");
+	public static final String MODID = "advancedtotems";
+	public static final String MODNAME = "Advanced Totems";
+
+	@Override
+	public void onInitialize() {
+		
+	}
+}
---
+++ src/main/java/com/rdm/advancedtotems/mixin/LivingEntityMixin.java
@@ -0,0 +1,10 @@
+package com.rdm.advancedtotems.mixin;
+
+import org.spongepowered.asm.mixin.Mixin;
+
+import net.minecraft.entity.LivingEntity;
+
+@Mixin(LivingEntity.class)
+public class LivingEntityMixin {
+	
+}
